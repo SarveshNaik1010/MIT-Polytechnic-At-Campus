c86207dab0a90a44f8c1f47c19f64ab18f9dac86
Sarvesh Naik <naiksarvesh463@gmail.com>
2024-01-18 09:09:35 +0530

    Added Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5501');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use('/api/v1/events', eventRouter);
 
 app.get('/api/v1/eventimgs/:img', async function (req, res, next) {
