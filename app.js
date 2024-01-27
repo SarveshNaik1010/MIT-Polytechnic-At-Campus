48dd989e0518c94b3169c865a01db9bff2555d06
Sarvesh Naik <naiksarvesh463@gmail.com>
2024-01-27 17:15:15 +0530

    Implamented cors

--- app.js
+++ app.js
@@ -4,28 +4,20 @@ const eventRouter = require('./router/eventRouter');
 const imgRouter = require('./router/imgRouter');
 const errorController = require('./controller/errorController');
 const AppError = require('./utils/appError');
+const cors = require('cors');
 
 const app = express();
 
 app.set('view engine', 'pug'); //No need to require
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(cors());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+app.use();
 
 // app.get('/demo', function(req, res, next) {
 //   res.status(200).render('demo');
